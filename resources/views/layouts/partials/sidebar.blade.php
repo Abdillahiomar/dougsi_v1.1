874e727aca73cc16d89c147322fe0bb04347a60e
ABDILLAHI OMAR DIRIEH <abdillahi.omar@djibtel.dj>
2026-08-10 21:02:29 +0300

    Sidebar fix finances.index menu

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -98,7 +98,7 @@
                     'Gestion' => [
                         [
                             'label'      => 'Finances',
-                            'route'      => 'finance.index',
+                            'route'      => 'finances.index',
                             'permission' => 'finance.view',
                             'svg'        => '<path stroke-linecap="round" stroke-linejoin="round" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"/>',
                         ],
