c0dca95d30519c0840670126920f5b35a29566c3
ABDILLAHI OMAR DIRIEH <abdillahi.omar@djibtel.dj>
2026-08-24 19:20:29 +0300

    update schoolClass model add use Illuminate\Database\Eloquent\Builder;

--- app/Models/SchoolClass.php
+++ app/Models/SchoolClass.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class SchoolClass extends Model
 {
